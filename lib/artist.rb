b77787d08c877e1d6e26de90ceb871136cba200d
Tony Ho <ho.tonym@gmail.com>
2018-05-17 01:15:27 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,30 @@
 require "pry"
 
 class Artist
-attr_accessor :name, :songs
+  attr_accessor :name, :songs
 
+  def initialize(name)
+     @name = name
+     @songs = []
+  end
 
- def initialize(name)
-   @name = name
-   @songs = []
- end
+   @@song_count = 0
 
- @@song_count = 0
+  def add_song(song)
+    @songs << song
+    @@song_count +=1
+    song.artist = self #adele
+  end
 
-def add_song(song)
-  @songs << song
-  @@song_count +=1
-  song.artist = self #adele
-end
+  def add_song_by_name(song_name)
+    the_song = Song.new(song_name)
 
-def add_song_by_name(song_name)
-  the_song = Song.new(song_name)
-  @songs << the_song
-  the_song.artist = self
-  @@song_count +=1
-end
+    the_song.artist = self
+    @@song_count +=1
+  end
 
-def self.song_count
-  @@song_count
-end
+  def self.song_count
+    @@song_count
+  end
 
 end
